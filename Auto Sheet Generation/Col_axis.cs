f4c08c658377ce2ec8a6c1233f9920844e356e37
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-04-13 10:43:58 +0200

    Add project files.

---
+++ Auto Sheet Generation/Col_axis.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Auto_Sheet_Generation
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Col_axis : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+
+            Document document = uIDocument.Document;
+            ViewFamilyType viewFamilyType = new FilteredElementCollector(document)
+                             .OfClass(typeof(ViewFamilyType)).Cast<ViewFamilyType>()
+                             .FirstOrDefault(x => ViewFamily.StructuralPlan == x.ViewFamily);
+            Level level = new FilteredElementCollector(document)
+                            .OfClass(typeof(Level)).Cast<Level>()
+                            .FirstOrDefault(x => x.Name == "02-_STR_FC");
+            try
+            {
+                using (Transaction transaction = new Transaction(document, "Sheet Generated"))
+                {
+                    transaction.Start();
+                    ViewPlan viewPlan = ViewPlan.Create(document, viewFamilyType.Id, level.Id);
+                    viewPlan.Name = "Columns and Axis Plan";
+
+                    List<BuiltInCategory> structuralCategories = new List<BuiltInCategory>
+                              {
+                                  BuiltInCategory.OST_StructuralFraming,
+                                  BuiltInCategory.OST_Floors,
+                                  BuiltInCategory.OST_StructuralFoundation,
+                                  BuiltInCategory.OST_FloorsStructure,
+                                  BuiltInCategory.OST_Walls,
+                                  BuiltInCategory.OST_StructuralFramingSystem,
+                                  BuiltInCategory.OST_Rebar,
+                                  BuiltInCategory.OST_HiddenStructuralFoundationLines,
+                                  BuiltInCategory.OST_AreaRein,
+                                  BuiltInCategory.OST_Elev,
+                                  BuiltInCategory.OST_Lines,
+                                  BuiltInCategory.OST_CLines,
+                              };
+
+                    foreach (BuiltInCategory category in structuralCategories)
+                    {
+                        ElementId categoryId = new ElementId((int)category);
+
+                        // Check if the category can be hidden in the view
+                        if (viewPlan.CanCategoryBeHidden(categoryId))
+                        {
+                            // Set the visibility of the category
+                            viewPlan.SetCategoryHidden(categoryId, true); // true = hide, false = unhide
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+
+            }
+        }
+    }
+}
+
---
+++ Auto Sheet Generation/ElevationMark.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Sheet_Generation
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ElevationMark : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+            Document document = uIDocument.Document;
+
+            List<ElevationMarker> elevationMarkers = new FilteredElementCollector(document)
+                .OfClass(typeof(ElevationMarker))
+                .Cast<ElevationMarker>()
+                .ToList();
+
+            List<ViewPlan> viewCollector = new FilteredElementCollector(document)
+                       .OfClass(typeof(ViewPlan)) // Filter specifically for ViewPlan
+                       .WhereElementIsNotElementType() // Exclude element types
+                       .Cast<ViewPlan>() // Cast to ViewPlan objects
+                       .Where(v => !v.IsTemplate) // Exclude templates 
+                       .ToList(); // Convert to a list
+            if (elevationMarkers == null)
+            {
+                message = "No elevation marker found!";
+                return Result.Failed;
+            }
+
+            BoundingBoxXYZ boundingBoxXYZ = viewCollector[0].get_BoundingBox(null);
+            try
+            {
+                using (Transaction transaction = new Transaction(document, "Sheet Generated"))
+                {
+
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
+
+
+
